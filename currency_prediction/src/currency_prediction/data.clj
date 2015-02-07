aeda52562a169080731fd009715831544a986129
Jelena Tabas <jelenatjelena@gmail.com>
2015-02-07 16:39:27 +0100

    readme

--- currency_prediction/src/currency_prediction/data.clj
+++ currency_prediction/src/currency_prediction/data.clj
@@ -2,6 +2,7 @@
    (:require [clj-http.client :as client]))
 
 (defn get-data []
-  (client/get "http://currencies.apps.grandtrunk.net/getrange/2012-01-01/2015-01-01/eur/rsd"))
+ (client/get "http://currencies.apps.grandtrunk.net/getrange/2012-01-01/2015-01-01/eur/rsd"))
 
 (get-data)
+
