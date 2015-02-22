2ccbb3d040363935ad32636ad127039e69804af3
Jelena Tabas <jelenatjelena@gmail.com>
2015-02-22 15:00:35 +0100

    fact string

--- currency_prediction/test/currency_prediction/data_test.clj
+++ currency_prediction/test/currency_prediction/data_test.clj
@@ -2,5 +2,5 @@
   (:require [midje.sweet :refer :all]
             [currency-prediction.data :refer :all]))
 
-(fact "string"
+(fact "make-map giving expected hash-map"
       (make-map  ["2012-06-02 117.593703915"] ) => {"2012-06-02" "117.593703915"})
