eb151382b602900dc5431b6718a862bfc576d1ab
Jelena Tabas <jelenatjelena@gmail.com>
2015-03-05 09:40:58 +0100

    approximation TODO

--- currency_prediction/src/currency_prediction/approximation.clj
+++ currency_prediction/src/currency_prediction/approximation.clj
@@ -1,7 +1,15 @@
 (ns currency-prediction.approximation)
 
-#_(defn 
-  
-  
-    )
+#_(defn  )
 
+
+
+
+;; TODO
+;; Get date from data
+;; Count date into time (milisec etc?) -> get values for x axis
+;;
+;; Get value from data
+;; Parse it into double -> get values for y axis
+;;
+;; Put it into map
