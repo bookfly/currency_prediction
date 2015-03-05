043b5b3132754a10088475bedc3e2e59b8ee9cdd
Jelena Tabas <jelenatjelena@gmail.com>
2015-03-05 18:38:59 +0100

    cleaning approximation.clj

--- currency_prediction/src/currency_prediction/approximation.clj
+++ currency_prediction/src/currency_prediction/approximation.clj
@@ -1,27 +1,16 @@
 (ns currency-prediction.approximation
+  (:require [currency-prediction.data :as data])
   (:require [clj-time [format :as f]])
   (:require [clj-time.core :as time])     
-  (:require [clj-time.coerce :as tc])
-  )
+  (:require [clj-time.coerce :as tc]))
   
-
-;; Get milliseconds since Unix epoch until today
-;; (http://stackoverflow.com/questions/17432032/how-do-i-get-a-unix-timestamp-in-clojure)
-(defn get-mills-since-ue []
-  (tc/to-long (time/now)))
-
 ;; Get date of the start of Unic epoch
 (defn get-epoch-date []
    (time/epoch))
 
-#_(get-mills-since-ue)
 #_(get-epoch-date)
 
 
-;; Get milliseconds since Unix epoch until the date provided
-;; (https://github.com/clj-time/clj-time)
-(defn get-mills-until-date [date]
-  (tc/to-long date))
 
 ;; Parse string into date
 (defn get-date-of-string [date]
@@ -31,40 +20,32 @@
 (defn get-date-of-date [date]
   (tc/from-date (get-date-of-string date)))
 
-#_(get-mills-until-date "2012-06-02")
 #_(get-date-of-string "2012-06-02")
-(get-date-of-date "2012-06-02")
+#_(get-date-of-date "2012-06-02")
 
 
-;; Get difference between current and given date/time
-(defn get-difference [date]
-  (- (get-mills-since-ue) (get-mills-until-date date)))
 
 ;; Get interval in days between two dates
 (defn get-difference-date [date]
   (time/in-days (time/interval  (get-epoch-date)  (get-date-of-date date))))
 
-#_(time/in-days (time/interval (time/date-time 1970 1 1) (time/date-time 2015 3 5)))
-#_(time/in-days (time/interval (get-epoch-date) (time/date-time 2014 3 5)))
-#_(time/in-days (time/interval (get-epoch-date) (get-date-of-date "2014-03-05")))
-
 #_(get-difference-date "2014-03-05")
 
 
 
-;; Look at: http://stackoverflow.com/questions/10752659/how-to-convert-a-numeric-string-to-number-decimal-and-number-to-string
 ;; Parse given string into double
+;; (http://stackoverflow.com/questions/10752659/how-to-convert-a-numeric-string-to-number-decimal-and-number-to-string)
 (defn parse-double [number]
   (read-string number))
 
 #_(parse-double "3245.23")
 
 
+;; Get hashMap from currency-prediction.data
+;; Parse values - dates and doubles
+;; Return into new hashMap
 
 
-;; Parse date to miliseconds - NOT USING!
-#_(defn parse-date-to-mills [date]
-   (tc/to-long date))
 
 
 ;; TODO
